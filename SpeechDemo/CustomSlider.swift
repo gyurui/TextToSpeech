2c06a0f4508058eeb90960fb67b4b383e02a655f
gyurui <gyuri.trum@gmail.com>
2017-06-23 09:47:21 +0200

    textToSpeech

---
+++ SpeechDemo/CustomSlider.swift
@@ -0,0 +1,29 @@
+//
+//  CustomSlider.swift
+//  SpeechDemo
+//
+//  Created by Gabriel Theodoropoulos on 10/2/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomSlider: UISlider {
+
+    var sliderIdentifier: Int!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        sliderIdentifier = 0        
+    }
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        sliderIdentifier = 0
+    }
+
+}
